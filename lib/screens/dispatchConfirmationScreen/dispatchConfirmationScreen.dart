c2f1545457a32a039d32e720cce95f37757f8d5c
abeselomg <abeselomgebrekidan12@gmail.com>
2021-10-26 16:53:26 +0300

    qr code scanner added

--- lib/screens/dispatchConfirmationScreen/dispatchConfirmationScreen.dart
+++ lib/screens/dispatchConfirmationScreen/dispatchConfirmationScreen.dart
@@ -1,6 +1,7 @@
 import 'package:cats_warehouse_mentor/constants/colors.dart';
 import 'package:cats_warehouse_mentor/constants/constants.dart';
 import 'package:cats_warehouse_mentor/screens/dispatchConfirmationScreen/widgets/commodityReportWidget.dart';
+import 'package:cats_warehouse_mentor/screens/dispatchConfirmationScreen/widgets/qrcode.dart';
 import 'package:flutter/material.dart';
 
 class DispatchConfirmationScreen extends StatefulWidget {
@@ -42,28 +43,7 @@ class _DispatchConfirmationScreenState
                     style: TextStyle(fontSize: kTitleBoldFont, color: kNavy)),
               ],
             ),
-            SizedBox(height: 8),
-            Container(
-              width: MediaQuery.of(context).size.width * 0.8,
-              child: TextField(
-                controller: textController,
-                decoration: InputDecoration(
-                  errorStyle: TextStyle(color: Colors.white),
-                  border: UnderlineInputBorder(
-                      borderRadius: BorderRadius.circular(4),
-                      borderSide: BorderSide(color: kNavy, width: 2)),
-                  focusedBorder: UnderlineInputBorder(
-                      borderRadius: BorderRadius.circular(4),
-                      borderSide: BorderSide(color: kNavy, width: 2)),
-                  enabledBorder: UnderlineInputBorder(
-                      borderRadius: BorderRadius.circular(4),
-                      borderSide: BorderSide(color: kNavy, width: 2)),
-                  hintText: "Confirmation Code",
-                  hintStyle: TextStyle(color: kNavy, fontSize: kNormalFont),
-                ),
-                keyboardType: TextInputType.text,
-              ),
-            ),
+  
             SizedBox(height: 16),
             Container(
               width: MediaQuery.of(context).size.width,
@@ -73,10 +53,14 @@ class _DispatchConfirmationScreenState
                     primary: kNavy,
                     shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(16))),
-                child: Text('Confirm',
+                child: Text('Scan Qr Code',
                     style:
                         TextStyle(fontSize: kButtonFont, color: Colors.white)),
-                onPressed: () {},
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => QRViewScreen(),
+                  ));
+                },
               ),
             ),
           ],
